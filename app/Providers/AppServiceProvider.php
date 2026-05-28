2bd26961a255a26da3c15dda669db00db95721d8
alexandreCaetanoUnipar <alexandre.caetano@edu.unipar.br>
2026-05-28 16:53:42 -0300

    Adição de logica Rate limit part 1
    colocando parte de codigo rate limit no appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Limite genérico para a API (60 requisições por minuto)
+    RateLimiter::for('api', function (Request $request) {
+        return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+    });
+
+    // Limite rigoroso para Login (5 tentativas por minuto) [1, 2]
+    RateLimiter::for('login', function (Request $request) {
+        return Limit::perMinute(5)->by($request->ip());
     }
 }
